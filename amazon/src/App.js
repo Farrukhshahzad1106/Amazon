d682b49bb2578a1296151a2e9a2f17014ad5ab12
Farrukh <shahzadfarrukh621@gmail.com>
2024-01-10 21:49:29 +0530

    Added the banner to the App

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 //We are importing BrowserRouter from reactrouter-dom but we are using it by the name of router here
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import Header from './Header';
+import Home from './Home';
 
 
 function App() {
@@ -9,10 +10,9 @@ function App() {
     <Router>
         <div className="App">
           <Routes>
-            <Route path='/' element={<Header/>}/>
+            <Route path='/' element={<Home/>}/>
             <Route path='/login' element={<div><Header/></div>} />
             <Route path='/checkout' element={<h1>Checkout Page</h1>}/>
-              {/* <Header/> */}
           </Routes>
         </div>
     </Router>
@@ -24,3 +24,5 @@ export default App;
 
 
 // BrowserRouter is a Higher-Order Component.
+//For the Banner we need a gradient effect which could be done that we have an image and we stck another white image on top of it whose opacity decreases as we need the effect of the gradient
+//The banner image is more visible at the top and gradually gradiates to white towards the bottom 
