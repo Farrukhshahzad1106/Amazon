2b03cd01c623682c84abecdd0fb12e0afd354423
Farrukh <shahzadfarrukh621@gmail.com>
2024-01-12 01:46:35 +0530

    Completed the Product Component

--- amazon/src/Home.js
+++ amazon/src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css'
 import Header from './Header';
+import Product from './Product';
 
 function Home() {
   return (
@@ -9,8 +10,63 @@ function Home() {
         <img src='https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg'
              alt='Amazon banner'
              className='home__image'/>
-        <h1>I am the home Page</h1>
+        <div className='home__row'>
+            <Product
+                id="12321341"
+                title="The Lean Startup"
+                price={11.96}
+                rating={5}
+                image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,201,203,200_.jpg"
+            />
+            <Product
+                id="12321341"
+                title="The Lean Startup"
+                price={11.96}
+                rating={5}
+                image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,201,203,200_.jpg"
+            />
+        </div>
+
+        <div className='home__row'>
+            <Product
+                id="12321341"
+                title="The Lean Startup"
+                price={11.96}
+                rating={5}
+                image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,201,203,200_.jpg"
+            />
+            <Product
+                id="12321341"
+                title="The Lean Startup"
+                price={11.96}
+                rating={5}
+                image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,201,203,200_.jpg"
+            />
+            <Product
+                id="12321341"
+                title="The Lean Startup"
+                price={11.96}
+                rating={5}
+                image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,201,203,200_.jpg"
+            />
+        </div>
+        <div className='home__row'>
+        <Product
+                id="12321341"
+                title="The Lean Startup"
+                price={11.96}
+                rating={5}
+                image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,201,203,200_.jpg"
+            />
+        </div>
+        
     </div>
   )
 }
 export default Home;
+//Product component will have the following:
+//Product ID
+//title
+// Price
+// Rating
+// Image
